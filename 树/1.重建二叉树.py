dd3cb03014e61725bbebe19422c2ba142646a922
strugglehonor <56100917+strugglehonor@users.noreply.github.com>
2021-04-28 09:24:06 +0800

    Update 1.重建二叉树.py

--- 树/1.重建二叉树.py
+++ 树/1.重建二叉树.py
@@ -9,7 +9,7 @@ class Solution:
     # 前序遍历第一个节点是根节点，根据根节点的值找到这个值在中序遍历里面的索引，
     # 然后划分为左右子树，递归重复这个过程
     def buildTree(self, preorder, inorder):
-        if len(inorder) == 0:
+        if len(inorder) == 0 and len(preorder)==0:
             return None
         root = TreeNode(preorder[0])
         mid = inorder.index(root.val)
