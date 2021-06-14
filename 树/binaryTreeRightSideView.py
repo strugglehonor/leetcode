c32c42e09820729765188dce750662d643d72d5e
strugglehonor <56100917+strugglehonor@users.noreply.github.com>
2021-06-14 16:43:29 +0800

    Create binaryTreeRightSideView.py

---
+++ 树/binaryTreeRightSideView.py
@@ -0,0 +1,15 @@
+class Solution:
+    def rightSideView(self, root: TreeNode) -> List[int]:
+        res = []
+        depth = 0
+        self.dfs(root, res, depth)
+        return res
+
+    def dfs(self, root, array, depth):
+        if root == None:
+            return 
+        if depth == len(array):
+            array.append(root.val)
+        depth += 1
+        self.dfs(root.right, array, depth)
+        self.dfs(root.left, array, depth)
