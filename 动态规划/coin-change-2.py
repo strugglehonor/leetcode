c776ff720deb509f91306fda5c51d6fe8a42a96e
strugglehonor <56100917+strugglehonor@users.noreply.github.com>
2021-06-24 00:33:52 +0800

    leetcode518.零钱兑换2

---
+++ 动态规划/coin-change-2.py
@@ -0,0 +1,9 @@
+# https://leetcode-cn.com/problems/coin-change-2/
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        dp = [0]*(amount+1)
+        dp[0] = 1
+        for coin in coins:
+            for i in range(coin, amount+1):
+                dp[i] += dp[i-coin]
+        return dp[amount]
